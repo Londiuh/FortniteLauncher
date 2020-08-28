1c794d52fe30ece983cf89c810edcf0f66391459
TheLondiux <gempleyero@gmail.com>
2020-08-28 14:49:33 +0200

    Improvments

--- FortniteLauncher/Program.cs
+++ FortniteLauncher/Program.cs
@@ -14,50 +14,15 @@ namespace FortniteLauncher
         public static readonly string shippingExe = $"{binPath}FortniteClient-Win64-Shipping.exe";
         public static readonly string eacShippingExe = $"{binPath}FortniteClient-Win64-Shipping_EAC.exe";
         //public static readonly string beShippingExe = $"{binPath}FortniteClient-Win64-Shipping_BE.exe";
+        //The obfuscationid changes in every fortnite update
         public static readonly string obfuscationid = "2DgGDiN4ZBB0TKHnkkCtGEJ2EzroWg";
         static Process _fnProcess;
         static Process _fnEacProcess;
         static Process _fnLauncher;
-
-        public static string GetToken(string authCode)
-        {
-            Console.WriteLine("Requesting access token...");
-            var cliente = new RestClient("https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token");
-            var peticion = new RestRequest(Method.POST);
-
-            peticion.AddHeader("Authorization", "basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=");
-            peticion.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-
-            peticion.AddParameter("grant_type", "authorization_code");
-            peticion.AddParameter("code", authCode);
-
-            string respuesta = cliente.Execute(peticion).Content;
-            var TokenAcceso = JsonSerializer.Deserialize<Token>(respuesta);
-            if (TokenAcceso.AccessToken != null)
-            {
-                Console.WriteLine("Access token successfully generated");
-                return TokenAcceso.AccessToken;
-            }
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(TokenAcceso.ErrorCode);
-            System.Threading.Thread.Sleep(5000);
-            Environment.Exit(0);
-            return "error";
-        }
-        
-        public static string GetExchange(string token)
-        {
-            Console.WriteLine("Requesting exchange code...");
-            var cliente = new RestClient("https://account-public-service-prod.ol.epicgames.com/account/api/oauth/exchange");
-            var peticion = new RestRequest(Method.GET);
-
-            peticion.AddHeader("Authorization", $"bearer {token}");
-
-            string respuesta = cliente.Execute(peticion).Content;
-            var exchangeCode = JsonSerializer.Deserialize<Exchange>(respuesta);
-            Console.WriteLine("Exchange code successfully generated");
-            return exchangeCode.Code;
-        }
+        // Default values, this values are changed if you auth
+        public static string token = "unused";
+        public static string exchange = "unused";
+        public static string authType = "unused";
 
         static void Main(string[] args)
         {
@@ -69,18 +34,18 @@ namespace FortniteLauncher
                 Thread.Sleep(3000);
                 Environment.Exit(2);
             }
-            string token = "unused";
-            string exchange = "unused";
-            string authType = "unused";
             Console.WriteLine("Do you want to authenticate? Y | N");
             if (Console.ReadKey().Key == ConsoleKey.Y)
             {
                 Console.WriteLine("Authorization requiered:");
-                token = GetToken(Console.ReadLine());
-                exchange = GetExchange(token);
+                token = Auth.GetToken(Console.ReadLine());
+                exchange = Auth.GetExchange(token);
                 authType = "exchangecode";
             }
+
+            //You don't really need to use the args for the launcher and the eac shipping but the EGL does that
             var launchArgs = $"-obfuscationid={obfuscationid} -AUTH_LOGIN=unused -AUTH_PASSWORD={exchange} -AUTH_TYPE={authType} -epicapp=Fortnite -epicenv=Prod -EpicPortal -noeac -nobe -fltoken=fdd9g715h4i20110dd40d7d3";
+            // I launch FortniteLauncher and FortniteClient-Win64-Shipping_EAC and freeze them to bypass the protection
             _fnLauncher = new Process
             {
                 StartInfo =
@@ -111,6 +76,7 @@ namespace FortniteLauncher
                 Win32.Thread_Suspend(Win32.Thread_GetHandle(thread.Id));
             }
 
+            //AsyncStreamReaders and RedirectStandardOutput to have fortnite output in the console
             _fnProcess = new Process
             {
                 StartInfo =
@@ -141,6 +107,7 @@ namespace FortniteLauncher
             asyncOutputReader.Start();
             asyncErrorReader.Start();
 
+            //If the game is closed kill the launcher and the eac shipping
             _fnProcess.WaitForExit();
             _fnLauncher.Kill();
             _fnEacProcess.Kill();
