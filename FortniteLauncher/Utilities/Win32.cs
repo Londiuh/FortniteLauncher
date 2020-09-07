af9644b70e511bc4676337f525811203d1e9293e
TheLondiux <gempleyero@gmail.com>
2020-09-07 13:41:07 +0200

    Added kernel32 functions for the DLL injector

--- FortniteLauncher/Utilities/Win32.cs
+++ FortniteLauncher/Utilities/Win32.cs
@@ -3,24 +3,33 @@ using System.Runtime.InteropServices;
 
 public static class Win32
 {
-    /*
-    TERMINATE = 0x0001
-    SUSPEND_RESUME = 0x0002
-    GET_CONTEXT = 0x0008
-    SET_CONTEXT = 0x0010
-    SET_INFORMATION = 0x0020
-    QUERY_INFORMATION = 0x0040
-    SET_THREAD_TOKEN = 0x0080
-    IMPERSONATE = 0x0100
-    DIRECT_IMPERSONATION = 0x0200
-    */
-
     [DllImport("kernel32.dll")]
     public static extern Int32 SuspendThread(IntPtr hThread);
+
     [DllImport("kernel32.dll")]
     public static extern int ResumeThread(IntPtr hThread);
+
     [DllImport("kernel32.dll")]
     public static extern IntPtr OpenThread(int dwDesiredAccess, bool bInheritHandle, int dwThreadId);
+
     [DllImport("kernel32.dll", SetLastError = true)]
     public static extern bool CloseHandle(IntPtr hHandle);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 }
